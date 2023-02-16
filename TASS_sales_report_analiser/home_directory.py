ac45a32ee4b231d98f36b5e6f088901d79e5313a
evgeniy <pavlinl4@gmail.com>
2023-02-16 22:59:35 +0300

    create sub folder in one of Users  folders

---
+++ TASS_sales_report_analiser/home_directory.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def subfolder_in_user_folder(subfolder):
+    return f'{Path.home()}/{subfolder}'
+
+
+if __name__ == '__main__':
+    print(subfolder_in_user_folder("Downloads"))
