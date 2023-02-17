bd944ab72a7a62d777866080570ecb5d76c660c3
evgeniy <pavlinbl4@gmail.com>
2023-02-17 20:59:38 +0300

    lite refactoring

--- TASS_sales_report_analiser/create_XLXS_report_file.py
+++ TASS_sales_report_analiser/create_XLXS_report_file.py
@@ -1,7 +1,6 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
 import os
-from datetime import datetime
 from pathlib import Path
 
 
@@ -11,7 +10,6 @@ def make_documents_subfolder(name):  # create folder "name" in User/Documents fo
 
 
 def create_report_file(report_folder, file_name, report_date):
-    # report_date = f'{report_date[1]}-{report_date[0]}'
     report_file_name = f"{file_name}.xlsx"
     path_to_file = f'{report_folder}/{report_file_name}'
 
@@ -23,9 +21,9 @@ def create_report_file(report_folder, file_name, report_date):
         ws = wb.active  # если файа еще нет
         ws.title = " ".join(report_date)  # если файда еще нет
 
-    ws.column_dimensions['A'].width = 20
-    ws.column_dimensions['B'].width = 20
-    ws.column_dimensions['C'].width = 20  # задаю ширину колонки
+    ws.column_dimensions['A'].width = 30
+    ws.column_dimensions['B'].width = 30
+    ws.column_dimensions['C'].width = 30  # задаю ширину колонки
 
     ws['A1'] = 'photo_id'  # create columns names
     ws['B1'] = 'income'
