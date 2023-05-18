6bea8dc2989223880da235c1116922b084614b85
evgeniy <pavlinbl4@gmail.com>
2023-05-18 10:00:31 +0300

    added function "get_html"

--- must_have/soup.py
+++ must_have/soup.py
@@ -1,5 +1,13 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from must_have.crome_options import setting_chrome_options
 
 
 def get_soup(html):
     return BeautifulSoup(html, 'lxml')
+
+def get_html(link):
+    browser = webdriver.Chrome(options=setting_chrome_options())
+    browser.get(link)
+    html = browser.page_source
+    return html
