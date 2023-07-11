f70fab5b9c0457c377889566e0e3075ae87833ae
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-07-11 22:03:59 +0300

    get_page_numbers - receive number of images on site

--- Images_on_site/count_images.py
+++ Images_on_site/count_images.py
@@ -1,4 +1,6 @@
+from must_have.crome_options import setting_chrome_options
 from must_have.soup import get_soup, get_html
+from selenium import webdriver
 
 
 def get_page_numbers(url, browser):  # get number of images on site
@@ -6,6 +8,15 @@ def get_page_numbers(url, browser):  # get number of images on site
     images_online = int(str(soup.select(".result-counter#nb-result"))[42:47])
     page_number = images_online // 20 + 1
     return page_number, images_online
+
+
+if __name__ == '__main__':
+    url_tass = 'https://shorturl.at/dCKLR'
+    browser = webdriver.Chrome(options=setting_chrome_options())
+    print(get_page_numbers(url_tass, browser))
+    browser.close()
+    browser.quit()
+
 
 
 
