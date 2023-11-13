887d4e1f0aace638523ebdd72e6f1e51c7a3c27e
evgeniy <pavlinbl4@gmail.com>
2023-11-13 17:47:50 +0300

    gui file selection

---
+++ html_report/gui_select_file.py
@@ -0,0 +1,15 @@
+from tkinter import filedialog, messagebox, Tk
+
+
+def select_file_via_gui():
+    root = Tk()
+    root.withdraw()
+
+    choose_file = filedialog.askopenfile(initialdir="/Users/evgeniy/Downloads", title="Select file")
+
+    if choose_file:
+        return choose_file.name
+    else:
+        messagebox.showwarning("Warning", "You haven't chosen a file. Program terminated.")
+
+    root.destroy()
