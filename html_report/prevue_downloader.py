50f8abb892c77ebac774b2b8dfdf684281245031
evgeniy <pavlinbl4@gmail.com>
2023-11-13 17:45:12 +0300

    add tqdm and modify downloaded imaqges filenames

--- html_report/prevue_downloader.py
+++ html_report/prevue_downloader.py
@@ -6,15 +6,16 @@ from must_have.crome_options import setting_chrome_options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import re
+from tqdm import tqdm
 
 
 def get_preview_mail_report(mail_report: dict, report_date: str):
     picture_folder = f'{"/Users/evgeniy/Library/Mobile Documents/com~apple~CloudDocs/TASS/"}{report_date}'
     os.makedirs(picture_folder, exist_ok=True)
     browser = webdriver.Chrome(options=setting_chrome_options())
-    for i in mail_report:
+    for i in tqdm(mail_report):
         if re.search(r'\d', mail_report[i][0]):
-            print(mail_report[i])
+            # print(mail_report[i])
             photo_id = mail_report[i][3]
             money = float(mail_report[i][5].replace(',', '.'))
             try:
@@ -28,9 +29,9 @@ def get_preview_mail_report(mail_report: dict, report_date: str):
                 )
 
                 picture = browser.find_element(By.CSS_SELECTOR, f"img.thumb{photo_id}").get_attribute("src")
-                print(picture)
+                # print(f'{picture = }')
                 get_image = requests.get(picture)
-                with open(f"{picture_folder}/{photo_id}_{money}.jpg", 'wb') as img_file:
+                with open(f"{picture_folder}/{photo_id}_{money}-({mail_report[i][0]}).jpg", 'wb') as img_file:
                     img_file.write(get_image.content)
             except Exception as ex:
                 print(ex)
