6be229880fd4112d0db4a56148662b7bede88546
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-05-28 23:22:35 +0300

    refactoring

--- TASS_sales_report_analiser/sale_report_analiser.py
+++ TASS_sales_report_analiser/sale_report_analiser.py
@@ -1,30 +1,15 @@
-import os
 import fnmatch
+import os
 import os.path
 import shutil
+
 import openpyxl
-from openpyxl import load_workbook
+
 from get_preview import get_preview
 from must_have.home_directory import subfolder_in_user_folder
+from tass_report_in_html_letter.write_to_xlsx import write_to_main_file
 from xlsx_tools.create_XLXS_report_file import create_report
 from xlsx_tools.read_XLSX_report import gen_x, gen_y
-
-
-def write_to_main_file(photos, main_report, report_date):  # записываю информацию в главный файл
-    wb = load_workbook(filename=main_report, read_only=False)
-    ws_month_number = wb.create_sheet(" ".join(report_date), 0)
-    ws_month_number.cell(row=1, column=1).value = "photo_id"
-    ws_month_number.cell(row=1, column=2).value = "income"
-    ws_month_number.cell(row=1, column=3).value = "sold times"
-
-    kkeys = [i for i in photos.keys()]
-    for i in range(len(photos)):
-        ws_month_number.cell(row=2 + i, column=1).value = kkeys[i]
-        ws_month_number.cell(row=2 + i, column=2).value = sum(photos[kkeys[i]])  # суммирую доход по снимкам
-        ws_month_number.cell(row=2 + i, column=3).value = len(photos[kkeys[i]])
-    wb.save(main_report)
-    wb.close()
-    print("информация записана")
 
 
 def add_information_to_main_file(file_to_work,
