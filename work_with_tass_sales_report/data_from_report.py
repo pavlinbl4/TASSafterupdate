2bc2a94cc2cca77d372226ee0d31f101ee5507e8
evgeniy <pavlinbl4@gmail.com>
2024-07-25 09:29:46 +0300

    make extract_money_value_from_report_file easy

--- work_with_tass_sales_report/data_from_report.py
+++ work_with_tass_sales_report/data_from_report.py
@@ -8,19 +8,18 @@ def get_info_from_report(mail_report: dict, file_extension: str) -> dict:
     for i in mail_report:
         logger.info(mail_report[i][0])
         if re.search(r'\d', mail_report[i][0]):
-            money, photo_id = extract_money_value_from_mail_report(file_extension, i, mail_report)
+            money, photo_id = extract_money_value_from_report_file(file_extension, i, mail_report)
             logger.info(f'{money = }')
             photos.setdefault(photo_id, [])
             photos[photo_id].append(money)
     return photos
 
 
-def extract_money_value_from_mail_report(file_extension, i, mail_report):
+def extract_money_value_from_report_file(file_extension, i, mail_report):
     photo_id = mail_report[i][3]
-    remove_spaces_and_comma_in_mail_report = None
+    column_number = 5  # for html report
     if file_extension == '.xlsx':
-        remove_spaces_and_comma_in_mail_report = mail_report[i][6].replace(' ', '').replace(',', '.')
-    elif file_extension == '.html':
-        remove_spaces_and_comma_in_mail_report = mail_report[i][5].replace(' ', '').replace(',', '.')
+        column_number = 6
+    remove_spaces_and_comma_in_mail_report = mail_report[i][column_number].replace(' ', '').replace(',', '.')
     money = float(remove_spaces_and_comma_in_mail_report)
     return money, photo_id
