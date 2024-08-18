c2895e8615c2ea480c0494f1e4311f38daad879f
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-18 22:21:57 +0300

    in old xlsx report money in 5 column and it is integer, I added lines but committed them

--- work_with_tass_sales_report/data_from_report.py
+++ work_with_tass_sales_report/data_from_report.py
@@ -7,7 +7,7 @@ def get_info_from_report(mail_report: dict, file_extension: str) -> dict:
     photos = {}
     for i in mail_report:
         logger.info(mail_report[i][0])
-        if re.search(r'\d', mail_report[i][0]):
+        if re.search(r'\d', str(mail_report[i][0])):
             money, photo_id = extract_money_value_from_mail_report(file_extension, i, mail_report)
             logger.info(f'{money = }')
             photos.setdefault(photo_id, [])
@@ -17,9 +17,13 @@ def get_info_from_report(mail_report: dict, file_extension: str) -> dict:
 
 def extract_money_value_from_mail_report(file_extension, i, mail_report):
     photo_id = mail_report[i][3]
+    logger.info(f"{photo_id = }")
     remove_spaces_and_comma_in_mail_report = None
     if file_extension == '.xlsx':
+        logger.info(f"{mail_report[i][6] = }")
+        # logger.info(f"{mail_report[i][5] = }, {type(mail_report[i][5])}")  # for old xlsx report - money in 5 column
         remove_spaces_and_comma_in_mail_report = mail_report[i][6].replace(' ', '').replace(',', '.')
+        # remove_spaces_and_comma_in_mail_report = mail_report[i][5]    # for old report money is integer
     elif file_extension == '.html':
         remove_spaces_and_comma_in_mail_report = mail_report[i][5].replace(' ', '').replace(',', '.')
     money = float(remove_spaces_and_comma_in_mail_report)
