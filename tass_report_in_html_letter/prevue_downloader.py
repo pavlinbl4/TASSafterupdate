582780781df477f30a883bdfca7b16f168f1c5cd
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-19 19:08:57 +0300

    download images from 'photos_report'

--- tass_report_in_html_letter/prevue_downloader.py
+++ tass_report_in_html_letter/prevue_downloader.py
@@ -12,19 +12,20 @@ from selenium.webdriver.chrome.service import Service
 from loguru import logger
 
 
-def get_preview_mail_report(mail_report: dict, report_date: str):
+def get_preview_mail_report(photos_report: dict, report_date: str):
     picture_folder = f'{"/Users/evgeniy/Library/Mobile Documents/com~apple~CloudDocs/TASS/"}{report_date}'
     os.makedirs(picture_folder, exist_ok=True)
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=setting_chrome_options())
 
-    for i in tqdm(mail_report):
-        logger.info(f"{mail_report[i][0] = } {type(mail_report[i][0]) = }")
-        if re.search(r'\d', str(mail_report[i][0])):
-            # print(mail_report[i])
-            photo_id = mail_report[i][3]
-            # money = float(mail_report[i][5].replace(' ', '').replace(',', '.')) # new report money type is string
-            money = float(mail_report[i][5])  # for old xlsx report - money type is integer
+    count = 1
+
+    for photo_id, income in tqdm(photos_report.items()):
+
+        for money in income:
+            rounded_money = f"{money:.2f}"
+            logger.info(f"{count} {photo_id = }, {rounded_money = }")
+
             try:
                 driver.get('https://www.tassphoto.com/ru')
                 WebDriverWait(driver, 10).until(
@@ -36,13 +37,18 @@ def get_preview_mail_report(mail_report: dict, report_date: str):
                 )
 
                 picture = driver.find_element(By.CSS_SELECTOR, f"img.thumb{photo_id}").get_attribute("src")
-                # print(f'{picture = }')
                 get_image = requests.get(picture)
-                with open(f"{picture_folder}/{photo_id}_{money}-({mail_report[i][0]}).jpg", 'wb') as img_file:
+                with open(f"{picture_folder}/{photo_id}_{rounded_money}-({count}).jpg", 'wb') as img_file:
                     img_file.write(get_image.content)
-            except Exception as ex:
-                print(ex)
-                driver.close()
-                driver.quit()
+                count += 1
+            except Exception as NoSuchElementException:
+                logger.info(NoSuchElementException)
+                logger.info(f"image {photo_id = } don't found")
+
     driver.close()
     driver.quit()
+
+
+if __name__ == '__main__':
+    photos = {1439890: [327.888324873], 45600520: [172.551020408], 53596363: [275.65, 345.692913385]}
+    get_preview_mail_report(photos, 'month and year')
