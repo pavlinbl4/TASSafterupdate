2d9298389e53b09834f6dc08cdb8fd5ca9990abb
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-19 19:07:20 +0300

    added filter to old type of xlsx report

--- work_with_tass_sales_report/data_from_report.py
+++ work_with_tass_sales_report/data_from_report.py
@@ -6,10 +6,9 @@ def get_info_from_report(mail_report: dict, file_extension: str) -> dict:
     # словарь, где ключ - id снимка, а значение список с цифрами покупок
     photos = {}
     for i in mail_report:
-        logger.info(mail_report[i][0])
+        # logger.info(mail_report[i][0])
         if re.search(r'\d', str(mail_report[i][0])):
             money, photo_id = extract_money_value_from_mail_report(file_extension, i, mail_report)
-            logger.info(f'{money = }')
             photos.setdefault(photo_id, [])
             photos[photo_id].append(money)
     return photos
@@ -20,11 +19,17 @@ def extract_money_value_from_mail_report(file_extension, i, mail_report):
     logger.info(f"{photo_id = }")
     remove_spaces_and_comma_in_mail_report = None
     if file_extension == '.xlsx':
-        logger.info(f"{mail_report[i][6] = }")
-        # logger.info(f"{mail_report[i][5] = }, {type(mail_report[i][5])}")  # for old xlsx report - money in 5 column
-        remove_spaces_and_comma_in_mail_report = mail_report[i][6].replace(' ', '').replace(',', '.')
-        # remove_spaces_and_comma_in_mail_report = mail_report[i][5]    # for old report money is integer
+        logger.info(f"File report is XLSX - {type(mail_report[i][5])}")
+        if mail_report[i][5] is int:
+            logger.info(
+                f"Old type report {mail_report[i][5] = }, {type(mail_report[i][5])}")  # for old xlsx report - money in 5 column
+            remove_spaces_and_comma_in_mail_report = mail_report[i][5]  # for old report money is integer
+        else:
+            logger.info(f" New type report {mail_report[i][6] = }")
+            remove_spaces_and_comma_in_mail_report = mail_report[i][6].replace(' ', '').replace(',', '.')
+
     elif file_extension == '.html':
         remove_spaces_and_comma_in_mail_report = mail_report[i][5].replace(' ', '').replace(',', '.')
+
     money = float(remove_spaces_and_comma_in_mail_report)
     return money, photo_id
