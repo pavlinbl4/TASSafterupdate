2e2eed5a581c481d6162cf948b73bdbef723116b
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-23 21:00:48 +0300

    refactoring - removed easy comments and logging

--- work_with_tass_sales_report/data_from_report.py
+++ work_with_tass_sales_report/data_from_report.py
@@ -1,12 +1,10 @@
 import re
-from loguru import logger
 
 
 def get_info_from_report(mail_report: dict, file_extension: str) -> dict:
     # словарь, где ключ - id снимка, а значение список с цифрами покупок
     photos = {}
     for i in mail_report:
-        # logger.info(mail_report[i][0])
         if re.search(r'\d', str(mail_report[i][0])):
             money, photo_id = extract_money_value_from_mail_report(file_extension, i, mail_report)
             photos.setdefault(photo_id, [])
@@ -16,16 +14,12 @@ def get_info_from_report(mail_report: dict, file_extension: str) -> dict:
 
 def extract_money_value_from_mail_report(file_extension, i, mail_report):
     photo_id = mail_report[i][3]
-    logger.info(f"{photo_id = }")
     remove_spaces_and_comma_in_mail_report = None
     if file_extension == '.xlsx':
-        logger.info(f"File report is XLSX - {type(mail_report[i][5])}")
+
         if isinstance(mail_report[i][5], (int, float)):
-            logger.info(
-                f"Old type report {mail_report[i][5] = }, {type(mail_report[i][5])}")  # for old xlsx report - money in 5 column
-            remove_spaces_and_comma_in_mail_report = mail_report[i][5]  # for old report money is integer
+            remove_spaces_and_comma_in_mail_report = mail_report[i][5]
         elif isinstance(mail_report[i][5], str):
-            logger.info(f" New type report {mail_report[i][6] = }")
             remove_spaces_and_comma_in_mail_report = mail_report[i][6].replace(' ', '').replace(',', '.')
 
     elif file_extension == '.html':
