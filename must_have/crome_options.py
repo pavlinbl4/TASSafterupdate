09f285aa508c995d557972be67c6fafb157edba5
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-23 21:44:47 +0300

    refactoring

--- must_have/crome_options.py
+++ must_have/crome_options.py
@@ -1,16 +1,48 @@
+from pathlib import Path
+
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import time
+
+
+def download_folder(path_to_download_folder=f'{Path.home()}/Downloads'):
+    preferences = {
+        "download.default_directory": path_to_download_folder
+    }
+    return preferences
 
 
 def setting_chrome_options():
     chrome_options = Options()
     chrome_options.add_experimental_option('detach', True)  # if browser close
     chrome_options.add_argument("--start-maximized")
-    # chrome_options.add_argument("--headless")  # фоновый режим
+    chrome_options.add_argument("--headless")  # фоновый режим
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # невидимость автоматизации
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
+    preferences = download_folder()
+    chrome_options = Options()
+
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    # chrome_options.add_experimental_option("prefs", preferences)
+    # # chrome_options.add_argument("webdriver.chrome.driver=chromedriver")
+    # # chrome_options.add_experimental_option('detach', True)
+
+    # chrome_options.add_argument("--ignore-certificate-errors")  # игнорирует ошибки сертификата SSL
+    # # chrome_options.add_argument("--disable-cache")  # отключает кэширование в браузере
+
+    # chrome_options.add_argument("--window-size=1280,800")
+    # chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # невидимость автоматизации
+    # chrome_options.add_argument(
+    #     "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
     return chrome_options
 
 
