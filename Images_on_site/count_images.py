38f227b36ea38a868593375f1a640c88172339d0
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-23 23:10:13 +0300

    улучшенный скрипт для проверки количества снимков для разных псевдонимов в агенстве

--- Images_on_site/count_images.py
+++ Images_on_site/count_images.py
@@ -1,18 +1,32 @@
-from must_have.crome_options import setting_chrome_options
-from must_have.soup import get_soup, get_html
+from urllib.parse import quote
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from must_have.crome_options import setting_chrome_options
+from must_have.soup import get_soup, get_html, browser_response
 
 
-def get_page_numbers(url, browser):  # get number of images on site
-    soup = get_soup(get_html(url, browser))
-    images_online = int(str(soup.select(".result-counter#nb-result"))[42:47])
-    page_number = images_online // 20 + 1
-    return page_number, images_online
+def get_image_numbers_with_selenium(url):  # get number of images on site
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=setting_chrome_options())
+    soup = get_soup(get_html(url, driver))
+    driver.close()
+    driver.quit()
+
+    images_online = soup.find('p', id='nb-result').get_text()
+    return images_online
+
+
+def get_image_numbers_with_soup(url):
+    soup = get_soup(browser_response(url))
+    images_online = soup.find('p', id='nb-result').text
+    return images_online
 
 
 if __name__ == '__main__':
-    url_tass = 'https://shorturl.at/dCKLR'
-    browser = webdriver.Chrome(options=setting_chrome_options())
-    print(get_page_numbers(url_tass, browser))
-    browser.close()
-    browser.quit()
+    url_tass = 'https://www.tassphoto.com/ru/asset/fullTextSearch/search/Валентин+Антонов/page/1'
+
+    encoded_url = quote(url_tass, safe=':/')
+    print(get_image_numbers_with_soup(encoded_url))
