90c5ad0eba7dde98c1cb4e10dcabbb7d551df81c
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-23 23:11:21 +0300

    добавлена библиотека requests и простейшая функция для нее

--- must_have/soup.py
+++ must_have/soup.py
@@ -1,11 +1,23 @@
 from bs4 import BeautifulSoup
+import requests
 
 
 def get_soup(html):
     return BeautifulSoup(html, 'lxml')
 
 
+def browser_response(url):
+    response = requests.get(url)
+    if response.ok:
+        return response.text
+
+
 def get_html(link, browser):
     browser.get(link)
     html = browser.page_source
     return html
+
+
+if __name__ == '__main__':
+    browser_response(
+        'https://www.tassphoto.com/ru/asset/fullTextSearch/search/%D0%B2%D0%B0%D0%BB%D0%B5%D0%BD%D1%82%D0%B8%D0%BD%20%D0%90%D0%BD%D1%82%D0%BE%D0%BD%D0%BE%D0%B2/page/1')
