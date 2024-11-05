620df6388b24d5795ac86cfd30290ecfec683fb9
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-11-05 23:08:18 +0300

    попытка сделать максимально лаконтчный скачивальщик превью

--- download_tass_preview.py
+++ download_tass_preview.py
@@ -1,3 +1,8 @@
+"""
+функция принимает photo_id и путь к сохраняемому файлу (так как файл может сохраняться
+под другим именем
+"""
+
 import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,21 +17,25 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=setting_chrome_options())
 
 
-def download_photo_preview_by_id(photo_id, path_to_download='test_downloads'):
+def download_photo_preview_by_id(photo_id: str, image_path: str):
     driver.get(f'https://www.tassphoto.com/ru/asset/fullTextSearch/search/{photo_id}/page/1')
     WebDriverWait(driver, 10).until(
         ec.presence_of_element_located((By.ID, "userrequest"))
     )
-    picture = driver.find_element(By.CSS_SELECTOR, f"img.thumb{photo_id}").get_attribute("src")
-    print(picture)
-    get_image = requests.get(picture)
-    os.makedirs(path_to_download,
-                exist_ok=True)
-    with open(
-            f"{path_to_download}/{photo_id}.jpg",
-            'wb') as img_file:
-        img_file.write(get_image.content)
+
+    picture_element = driver.find_element(By.CSS_SELECTOR, f"img.thumb{photo_id}")
+    picture_url = picture_element.get_attribute("src")
+
+    image_response = requests.get(picture_url)
+
+    with open(image_path, 'wb') as img_file:
+        img_file.write(image_response.content)
 
 
 if __name__ == '__main__':
-    download_photo_preview_by_id(photo_id='75520997')
+    picture_folder = 'test_downloads'
+    image_file_name = 'renamed_jpeg_image.jpg'
+    _image_path = os.path.join(picture_folder, image_file_name)
+    os.makedirs(picture_folder,
+                exist_ok=True)
+    download_photo_preview_by_id('75520997', _image_path)
