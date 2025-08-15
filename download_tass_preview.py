a72385c8e5a67177636734051c5116ad382f290e
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-08-15 12:09:50 +0300

    added ConfigLoader

--- download_tass_preview.py
+++ download_tass_preview.py
@@ -7,11 +7,18 @@ import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+
+from core.config_loader import ConfigLoader
 from must_have.crome_options import setting_chrome_options
 import requests
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
+from loguru import logger
+# Константы
+config = ConfigLoader.load("TASS")
+logger.info(config["TASS_URL"])
+
 
 
 def download_photo_preview_by_id(photo_id: str, picture_folder_downloads: str, image_file_name=None):
@@ -28,7 +35,7 @@ def download_photo_preview_by_id(photo_id: str, picture_folder_downloads: str, i
 
     try:
         # Открытие страницы с фотографией
-        driver.get(f'https://www.tassphoto.com/ru/asset/fullTextSearch/search/{photo_id}/page/1')
+        driver.get(f'{config["TASS_URL"]}/asset/fullTextSearch/search/{photo_id}/page/1')
         WebDriverWait(driver, 10).until(
             ec.presence_of_element_located((By.ID, "userrequest"))
         )
@@ -56,6 +63,6 @@ def download_photo_preview_by_id(photo_id: str, picture_folder_downloads: str, i
         driver.quit()
 
 
-if __name__ == '__main__':
-
-    download_photo_preview_by_id('73669467', 'test_downloads', 'October_best_picture.jpg')
+# if __name__ == '__main__':
+#
+#     download_photo_preview_by_id('73669467', 'test_downloads', 'October_best_picture.jpg')
